2fa4d99e25aa277894f46a8bcf07b50ff9aeff07
Ngoga Innocent <ngogainnocent1@gmail.com>
2024-02-06 20:51:31 +0200

    gunicorn

--- MureSounds/settings.py
+++ MureSounds/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 }
 #DATABASES['default']=dj_database_url.parse('postgres://muresoundstudio_user:1wvUHEO6q1GOyqYjlUByFGjDOZqlKObD@dpg-cn15vmmn7f5s73fe5ad0-a.oregon-postgres.render.com/muresoundstudio')
 database_url=os.environ.get("DATABASE_URL")
-DATABASES["default"]=dj_database_url(database_url)
+DATABASES["default"]=dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 

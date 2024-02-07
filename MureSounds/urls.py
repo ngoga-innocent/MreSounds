01f8040e64027825337de177a62b3581bbc0ad25
Ngoga Innocent <ngogainnocent1@gmail.com>
2024-02-07 12:13:15 +0200

    statics

--- MureSounds/urls.py
+++ MureSounds/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
      path("__reload__/", include("django_browser_reload.urls")),
@@ -10,3 +11,5 @@ urlpatterns = [
      path('staff/',include('Staff.urls')),
      path('accounts/',include('Accounts.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns +=staticfiles_urlpatterns()
